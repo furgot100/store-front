d152c39521d92f1b7e08e11d1de765c093e69e83
Francis Tsang <frtsang40@gmail.com>
2019-12-09 09:36:41 -0800

    init

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, redirect, url_for
+from pymongo import MongoClient
+# from bson.objectid import ObjectId
+
+client = MongoClient()
+db = client.Store
+item = db.items
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    '''Returns the homepage'''
+    return render_template('index.html')
+
+@app.route('/item/new')
+def new_item():
+    return render_template('new_item.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
