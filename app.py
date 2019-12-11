412dc60c1c6439b08bf8f080d3a56440d36a1167
Francis Tsang <frtsang40@gmail.com>
2019-12-11 11:15:12 -0800

    new routes

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def index():
 
 @app.route('/item/new')
 def new_item():
-    return render_template('new_item.html', item=items)
+    return render_template('new_item.html')
 
 
 @app.route('/item', methods=['POST'])
@@ -27,14 +27,34 @@ def item_submit():
         'description' : request.form.get('description'),
         'img_url': request.form.get('img_url')
     }
-    items.insert_one(item)
-    return redirect(url_for('index'))
+    item_id = items.insert_one(item).inserted_id
+    return redirect(url_for('index',item_id=item_id))
 
 
 @app.route('/item/<item_id>')
 def show_item(item_id):
     item = items.find_one({'_id': ObjectId(item_id)})
     return render_template('show_item.html', item=item)
+
+
+@app.route('/edit/<item_id>', methods=['POST'])
+def update_item(item_id):
+    
+    new_item = {
+        'name': request.form.get('name'),
+        'description': request.form.get('description'),
+        'img_url': request.form.get('img_url')
+    }
+    items.update_one(
+        {'_id': ObjectId(item_id)},
+        {'$set': new_item}
+    )
+    return redirect(url_for('show_item', item_id=item_id))
+
+@app.route('/edit/<item_id>', methods=['GET'])
+def edit_item(item_id):
+    item = items.find_one({'_id': ObjectId(item_id)})
+    return render_template('edit_item.html', item=item)
 
 
 
